c9af2099a36070f45e35b9787fec032355b2ef3b
Victor Dallé (dalle5u) <victor.dalle8@etu.univ-lorraine.fr>
2022-02-02 16:00:00 +0100

    Test de la fusion

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int a = 5;
+        System.out.println("Hello World" + a);
     }
 }

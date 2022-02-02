e0f74c6e2406d62f071d3a269a79b3c26bf9532c
Victor Dallé (dalle5u) <victor.dalle8@etu.univ-lorraine.fr>
2022-02-02 16:03:58 +0100

    Fusion avec le main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 5;
-        System.out.println("Hello World" + a);
+        System.out.println("Hello World");
     }
 }

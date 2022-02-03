efd9add99c225b403de7a4ccd51c59c3056b93ba
kurth2u <kurth2u@etu.univ-lorraine.fr>
2022-02-03 10:31:05 +0100

    class ActiviteRestreinte, SasEntree, SasSortie

---
+++ src/twisk/monde/ActiviteRestreinte.java
@@ -0,0 +1,12 @@
+package twisk.monde;
+
+public class ActiviteRestreinte extends Activite {
+
+    public ActiviteRestreinte(String nom){
+        super(nom);
+    }
+
+    public ActiviteRestreinte(String nom, int t, int e){
+        super(nom,t,e);
+    }
+}

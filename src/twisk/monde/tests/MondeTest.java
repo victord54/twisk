8d93dd1f40c468753935d6c98dbf971fbae62309
kurth2u <kurth2u@etu.univ-lorraine.fr>
2022-02-06 18:10:10 +0100

    Ajout class MondeTest

---
+++ src/twisk/monde/tests/MondeTest.java
@@ -0,0 +1,70 @@
+package twisk.monde.tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import twisk.monde.Activite;
+import twisk.monde.Etape;
+import twisk.monde.Guichet;
+import twisk.monde.Monde;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MondeTest {
+    Monde monde;
+    Monde monde2;
+
+    @BeforeEach
+    void setUp() {
+        monde = new Monde();
+        monde2 = new Monde();
+    }
+
+    @Test
+    void aCommeEntree() {
+        int nbEntree = monde.getSasEntree().nbSuccesseurs();
+        assertEquals(0,nbEntree);
+        monde.aCommeEntree(new Activite("e"), new Activite("ee"));
+        nbEntree = monde.getSasEntree().nbSuccesseurs();
+        assertEquals(2,nbEntree);
+    }
+
+    @Test
+    void aCommeSortie() {
+        Etape e1 = new Activite("e");
+        Etape e2 = new Activite("ee");
+        Etape e3 = new Activite("eee");
+        Etape e4 = new Activite("eeee");
+        monde.ajouter(e1,e2,e3,e4);
+        monde.aCommeSortie(e3,e4);
+
+
+    }
+
+    @Test
+    void ajouter() {
+        assertEquals(0,monde.nbEtapes());
+        monde.ajouter(new Activite("e"), new Activite("ee"), new Activite("eee"));
+        assertEquals(3,monde.nbEtapes());
+    }
+
+    @Test
+    void nbEtapes() {
+        assertEquals(0,monde.nbEtapes());
+        monde.ajouter(new Activite("e"), new Activite("ee"), new Activite("eee"));
+        assertEquals(3,monde.nbEtapes());
+    }
+
+    @Test
+    void nbGuichets() {
+        assertEquals(0,monde.nbGuichets());
+        monde.ajouter(new Activite("e"), new Guichet("ee"), new Activite("eee"));
+        assertEquals(1,monde.nbGuichets());
+        monde2.ajouter(new Guichet("e"), new Guichet("ee"), new Guichet("eee"));
+        assertEquals(3,monde2.nbGuichets());
+
+    }
+
+    @Test
+    void iterator() {
+    }
+}

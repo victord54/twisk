dbbd30cba7ab3cdd65f2291d91c033432ec2b1a5
kurth2u <kurth2u@etu.univ-lorraine.fr>
2022-02-06 18:22:10 +0100

    ajout class Simulation + simuler(Monde monde)

---
+++ src/simulation/Simulation.java
@@ -0,0 +1,12 @@
+package simulation;
+
+import twisk.monde.Monde;
+
+public class Simulation {
+    public Simulation(){}
+
+    public void simuler(Monde monde){
+        System.out.println(monde.toString());
+    }
+
+}

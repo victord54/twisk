b7d8561ee91dc206e534e4fa209fda2f19de101b
Victor Dallé (dalle5u) <victor.dalle8@etu.univ-lorraine.fr>
2022-02-22 15:22:11 +0100

    ajout fichiers c

---
+++ twisk-c/client.c
@@ -0,0 +1,20 @@
+#include "def.h"
+
+// Constantes de client.c
+#define ENTREE 0
+#define GUICHET 1
+#define ACTIVITE 2
+#define SORTIE 3
+
+#define SEMAPHORE 1
+
+void simulation(int ids) {
+    entrer(ENTREE);
+    delai(3, 1);
+    transfert(ENTREE, GUICHET);
+    P(ids, SEMAPHORE);
+    transfert(GUICHET, ACTIVITE);
+    delai(3,2);
+    V(ids, SEMAPHORE);
+    transfert(ACTIVITE, SORTIE);
+}

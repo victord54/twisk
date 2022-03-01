5f53918231f788dc3cfd01026e53f881292a95d5
kurth2u <kurth2u@etu.univ-lorraine.fr>
2022-03-01 14:13:21 +0100

    toC Ac Restreinte

--- src/twisk/monde/ActiviteRestreinte.java
+++ src/twisk/monde/ActiviteRestreinte.java
@@ -9,4 +9,12 @@ public class ActiviteRestreinte extends Activite {
     public ActiviteRestreinte(String nom, int t, int e) {
         super(nom,t,e);
     }
+
+    @Override
+    public String toC(){
+        StringBuilder builder = new StringBuilder();
+        builder.append("delai(" + temps + "," + ecartTemps +");\n");
+        builder.append("transfert(" + this.numEtape + "," + this.gestionnaireSuccesseur.getSucc(0).getNom() + ");\n");
+        return builder.toString();
+    }
 }

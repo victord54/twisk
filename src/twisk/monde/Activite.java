e2cf0b7db0f21e2519112dba8c19e93aaed8f049
Victor Dallé (dalle5u) <victor.dalle8@etu.univ-lorraine.fr>
2022-03-15 16:25:29 +0100

    Documentation pour Activite.

--- src/twisk/monde/Activite.java
+++ src/twisk/monde/Activite.java
@@ -1,26 +1,55 @@
+/**
+ * Classe représentant une activité du monde.
+ *
+ * @author Kurth Claire et Dallé Victor
+ * @since 02/02/2022
+ */
+
 package twisk.monde;
 
 public class Activite extends Etape {
     protected int temps;
     protected int ecartTemps;
 
+    /**
+     * Constructeur de la classe.
+     *
+     * @param nom Nom de l'activité.
+     */
     public Activite(String nom) {
         super(nom);
         temps = 4;
         ecartTemps = 2;
     }
 
+    /**
+     * Autre constructeur de la classe.
+     *
+     * @param nom Nom de l'activité.
+     * @param t   Durée de l'activité.
+     * @param e   Incertitude sur la durée (t +/- e)
+     */
     public Activite(String nom, int t, int e) {
         super(nom);
         this.temps = t;
         this.ecartTemps = e;
     }
 
+    /**
+     * Méthode qui vérifie si une étape est une activité.
+     *
+     * @return Un booléen qui renvoie true car c'est une activité.
+     */
     @Override
     public boolean estUneActivite() {
         return true;
     }
 
+    /**
+     * Méthode définissant le code c à ajouter pour une activité.
+     *
+     * @return Le code c.
+     */
     @Override
     public String toC() {
         StringBuilder builder = new StringBuilder();
@@ -34,7 +63,13 @@ public class Activite extends Etape {
         return builder.toString();
     }
 
-    public int getNbJetons(){
+    /**
+     * Méthode donnant le nombre de jetons.
+     *
+     * @return 0, car c'est une activité et non un guichet.
+     */
+    @Override
+    public int getNbJetons() {
         return 0;
     }
 }

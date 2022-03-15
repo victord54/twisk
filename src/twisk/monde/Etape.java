100b6f9c1b38da6a56e73ec3c7896aa6c7211157
Victor Dallé (dalle5u) <victor.dalle8@etu.univ-lorraine.fr>
2022-03-15 16:04:49 +0100

    Documentation pour Etape.

--- src/twisk/monde/Etape.java
+++ src/twisk/monde/Etape.java
@@ -1,3 +1,9 @@
+/**
+ * Classe abstraite représentant une étape du monde.
+ *
+ * @author Kurth Claire et Dallé Victor
+ * @since 02/02/2022
+ */
 package twisk.monde;
 
 import twisk.outils.FabriqueNumero;
@@ -9,28 +15,58 @@ public abstract class Etape implements Iterable<Etape> {
     protected String nom;
     protected GestionnaireSuccesseur gestionnaireSuccesseur;
 
+    /**
+     * Constructeur de la classe
+     *
+     * @param nom Nom de l'étape.
+     */
     public Etape(String nom) {
         this.nom = nom;
         gestionnaireSuccesseur = new GestionnaireSuccesseur();
         numEtape = FabriqueNumero.getInstance().getNumeroEtape();
     }
 
+    /**
+     * Méthode qui ajoute un successeur à l'étape actuelle.
+     *
+     * @param etapes Les étapes successeures.
+     */
     public void ajouterSuccesseur(Etape... etapes) {
         gestionnaireSuccesseur.ajouter(etapes);
     }
 
+    /**
+     * Méthode qui donne le nombre de successeurs de l'étape actuelle.
+     *
+     * @return Le nombre de successeurs.
+     */
     public int nbSuccesseurs() {
         return gestionnaireSuccesseur.nbEtapes();
     }
 
+    /**
+     * Méthode qui vérifie si l'étape est une activité.
+     *
+     * @return Un booléen indiquant si c'est une activité ou non.
+     */
     public boolean estUneActivite() {
         return false;
     }
 
+    /**
+     * Méthode qui vérifie si l'étape est un guichet.
+     *
+     * @return Un booléen indiquant si c'est un guichet ou non.
+     */
     public boolean estUnGuichet() {
         return false;
     }
 
+    /**
+     * Méthode qui vérifie si l'étape est une sortie.
+     *
+     * @return Un booléen indiquant si c'est une sortie ou non.
+     */
     public boolean estUneSortie() {
         for (Etape etape : gestionnaireSuccesseur) {
             if (etape.getNom().equalsIgnoreCase("sortie")) {
@@ -40,22 +76,47 @@ public abstract class Etape implements Iterable<Etape> {
         return false;
     }
 
+    /**
+     * Méthode définissant un nouvel itérateur pour Etape
+     *
+     * @return Le nouvel itérateur
+     */
     public Iterator<Etape> iterator() {
         return gestionnaireSuccesseur.iterator();
     }
 
-    public String getNom(){
+    /**
+     * Getter du nom de l'activité.
+     *
+     * @return Le nom.
+     */
+    public String getNom() {
         return this.nom;
     }
 
-    public int getNumEtape(){
+    /**
+     * Getter du numéro de l'étape.
+     *
+     * @return Le numéro de l'étape.
+     */
+    public int getNumEtape() {
         return numEtape;
     }
 
-    public String toString(){
+    /**
+     * Méthode toString().
+     *
+     * @return Les champs constituant l'étape.
+     */
+    public String toString() {
         return this.nom + " : " + nbSuccesseurs() + " successeur(s) " + gestionnaireSuccesseur.toString();
     }
 
+    /**
+     * Méthode définissant le code c à ajouter pour une étape.
+     *
+     * @return Le code C.
+     */
     public abstract String toC();
 
 

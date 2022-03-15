d3ae12b55d07cff9276d5e178dc824c2e6d521f9
kurth2u <kurth2u@etu.univ-lorraine.fr>
2022-03-15 14:10:45 +0100

    déclaration fonctions natives

--- src/twisk/simulation/Simulation.java
+++ src/twisk/simulation/Simulation.java
@@ -4,6 +4,10 @@ import twisk.monde.Monde;
 import twisk.outils.KitC;
 
 public class Simulation {
+    public native int [] start_simulation(int nbEtapes, int nbGuichets, int nbClients, int tabJetonsGuichet[]);
+    public native void nettoyage();
+    public native int [] ou_sont_les_clients(int nbEtapes, int nbClients);
+
     private KitC kit;
 
     public Simulation() {

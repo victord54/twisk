f30084373cead426edc3460502a70ca27798d89c
Victor Dallé (dalle5u) <victor.dalle8@etu.univ-lorraine.fr>
2022-03-15 16:50:29 +0100

    Documentation pour GestionnaireSuccesseur.

--- src/twisk/monde/GestionnaireSuccesseur.java
+++ src/twisk/monde/GestionnaireSuccesseur.java
@@ -1,3 +1,10 @@
+/**
+ * Classe représentant le gestionnaire des successeurs des étapes.
+ *
+ * @author Kurth Claire et Dallé Victor
+ * @since 02/02/2022
+ */
+
 package twisk.monde;
 
 import java.util.ArrayList;
@@ -5,40 +12,77 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 public class GestionnaireSuccesseur implements Iterable<Etape> {
+    /**
+     * ArrayList contenant les successeurs d'une étape.
+     */
     private final ArrayList<Etape> etapes;
 
+    /**
+     * Constructeur de la classe.
+     */
     public GestionnaireSuccesseur() {
         etapes = new ArrayList<>();
     }
 
+    /**
+     * Méthode qui permet d'ajouter des étapes au gestionnaire.
+     *
+     * @param etapes Les successeurs.
+     */
     public void ajouter(Etape... etapes) {
         this.etapes.addAll(Arrays.asList(etapes));
     }
 
+    /**
+     * Getter qui donne le nombre de successeurs de l'étape.
+     *
+     * @return Le nombre de successeurs.
+     */
     public int nbEtapes() {
         return etapes.size();
     }
 
-
+    /**
+     * Méthode qui définie un nouvel itérateur pour le gestionnaire de successeurs.
+     *
+     * @return Le nouvel itérateur.
+     */
     @Override
     public Iterator<Etape> iterator() {
         return etapes.iterator();
     }
 
+    /**
+     * Méthode toString()
+     *
+     * @return Les successeurs de l'étape concernée.
+     */
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
         for (Etape e : etapes) {
             builder.append(" - ").append(e.getNom());
         }
-        
+
         return builder.toString();
     }
+
+    /**
+     * Getter qui donne un successeur.
+     *
+     * @param i Indice de l'étape à renvoyer.
+     * @return L'étape à l"indice i.
+     */
     public Etape getEtape(int i) {
         return etapes.get(i);
     }
 
-    public Etape getSucc(){
+    /**
+     * Getter qui donne le premier successeur.
+     *
+     * @return Le 1er successeur.
+     */
+    public Etape getSucc() {
         return getEtape(0);
     }
 

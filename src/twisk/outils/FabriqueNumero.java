5a5235f9eb15e1a002ec508e08126ddfc04ceb59
Victor Dallé (dalle5u) <victor.dalle8@etu.univ-lorraine.fr>
2022-03-20 13:13:35 +0100

    Ajout doc pour FabriqueNumero

--- src/twisk/outils/FabriqueNumero.java
+++ src/twisk/outils/FabriqueNumero.java
@@ -1,31 +1,64 @@
+/**
+ * Classe représentant la fabrique de numéro pour les étapes et les guichets.
+ *
+ * @author Kurth Claire et Dallé Victor
+ * @since 02/02/2022
+ */
 package twisk.outils;
 
 public class FabriqueNumero {
+    /**
+     * Compteur d'étapes.
+     */
     private int cptEtape;
+
+    /**
+     * Compteur de guichets.
+     */
     private int cptSemaphore;
+
+    /**
+     * Instante pour le singleton.
+     */
     private static final FabriqueNumero instance = new FabriqueNumero();
 
+    /**
+     * Constructeur par défaut de la classe.
+     */
     private FabriqueNumero() {
     }
 
+    /**
+     * Getter donnant l'instance du singleton.
+     * @return instance.
+     */
     public static FabriqueNumero getInstance() {
         return instance;
     }
 
+    /**
+     * Getter donnant le numéro unique de la prochaine étape.
+     * @return Numéro unique de l'étape.
+     */
     public int getNumeroEtape() {
         cptEtape++;
         return cptEtape - 1;
     }
 
+    /**
+     * Getter donnant le numéro unique du prochain guichet.
+     * @return Sémaphore unique.
+     */
     public int getNumeroSemaphore() {
         cptSemaphore++;
         return cptSemaphore;
     }
 
+    /**
+     * Méthode remettant à 0 les compteurs d'étapes et de sémaphore.
+     */
     public void reset() {
         cptEtape = 0;
         cptSemaphore = 0;
     }
-
-
 }

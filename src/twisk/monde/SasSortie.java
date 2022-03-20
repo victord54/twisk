87bf7f8c890e78adf7139dc7353b32a6b20e34f3
Victor Dallé (dalle5u) <victor.dalle8@etu.univ-lorraine.fr>
2022-03-20 13:09:17 +0100

    Ajout doc pour SasSortie

--- src/twisk/monde/SasSortie.java
+++ src/twisk/monde/SasSortie.java
@@ -1,11 +1,25 @@
+/**
+ * Classe représentant un sas de sortie.
+ *
+ * @author Kurth Claire et Dallé Victor
+ * @since 02/02/2022
+ */
+
 package twisk.monde;
 
 public class SasSortie extends Activite {
 
+    /**
+     * Constructeur par défaut de la classe.
+     */
     public SasSortie() {
         super("sortie");
     }
 
+    /**
+     * Méthode définissant le code c à ajouter pour un sas de sortie.
+     * @return Le code c.
+     */
     public String toC(){
         return "";
     }

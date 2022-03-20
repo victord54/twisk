705d5e6f8fa623446267d9e2cc9b83b7e9e68ecd
Victor Dallé (dalle5u) <victor.dalle8@etu.univ-lorraine.fr>
2022-03-20 12:54:48 +0100

    Ajout doc pour Guichet

--- src/twisk/monde/Guichet.java
+++ src/twisk/monde/Guichet.java
@@ -1,32 +1,68 @@
+/**
+ * Classe représentant Un guichet.
+ *
+ * @author Kurth Claire et Dallé Victor
+ * @since 02/02/2022
+ */
+
 package twisk.monde;
 
 import twisk.outils.FabriqueNumero;
 
 public class Guichet extends Etape {
+    /**
+     * Champs contenant le nombre de jetons disponibles.
+     */
     protected int nbJetons;
+
+    /**
+     * Champs contenant l'identifiant unique du guichet.
+     */
     protected int numSemaphore;
 
+    /**
+     * Constructeur de la classe.
+     * @param nom Nom associé au guichet.
+     */
     public Guichet(String nom) {
         super(nom);
         numSemaphore = FabriqueNumero.getInstance().getNumeroSemaphore();
         nbJetons = 3;
     }
 
+    /**
+     * Constructeur de la classe.
+     * @param nom Nom associé au guichet.
+     * @param nb Nombres de jetons pour le guichet.
+     */
     public Guichet(String nom, int nb) {
         super(nom);
         this.nbJetons = nb;
         numSemaphore = FabriqueNumero.getInstance().getNumeroSemaphore();
     }
 
+    /**
+     * Méthode vérifiant si l'Etape est un guichet.
+     * @return Vrai car c'est un guichet.
+     */
     @Override
     public boolean estUnGuichet() {
         return true;
     }
 
+    /**
+     * Getter donnat l'id du guichet.
+     * @return Numéro de sémaphore.
+     */
     public int getNumSemaphore() {
         return numSemaphore;
     }
 
+    /**
+     * Méthode abstraite définissant le code c à ajouter pour une étape.
+     *
+     * @return Le code c.
+     */
     @Override
     public String toC() {
         StringBuilder str = new StringBuilder();
@@ -35,6 +71,10 @@ public class Guichet extends Etape {
         return str.toString();
     }
 
+    /**
+     * Getter donnant le nombre de jetons disponibles pour le guichet.
+     * @return Le nombre de jetons.
+     */
     public int getNbJetons(){
         return nbJetons;
     }

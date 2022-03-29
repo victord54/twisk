8c6dec510497dd0beb8ba2bd0d1b50de3d2cc788
kurth2u <kurth2u@etu.univ-lorraine.fr>
2022-03-29 14:16:09 +0200

    classe GestionnaireClients

---
+++ src/twisk/simulation/GestionnaireClients.java
@@ -0,0 +1,46 @@
+package twisk.simulation;
+
+import twisk.monde.Etape;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class GestionnaireClients implements Iterable<Client> {
+    protected ArrayList<Client> clients;
+    protected int nbClients;
+
+    public GestionnaireClients(){
+        this.clients = new ArrayList<>();
+    }
+
+    public GestionnaireClients(int nbClients){
+        this.clients = new ArrayList<>();
+        this.nbClients = nbClients;
+    }
+
+    public void setClients(int ... nbClients){
+        for (int n : nbClients){
+            this.clients.add(new Client(n));
+        }
+    }
+
+    public void setNbClients(int nbClients){
+        this.nbClients = nbClients;
+    }
+
+
+    public void allerA(int numeroClient, Etape etape, int rang){
+        
+    }
+
+    public void nettoyer(){
+        this.clients.clear();
+    }
+
+    @Override
+    public Iterator<Client> iterator(){
+        this.clients.iterator();
+    }
+
+    
+}

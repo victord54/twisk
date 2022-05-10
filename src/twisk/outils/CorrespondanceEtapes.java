1cb42a7eb123579e8e324e69b9d8f8f75941657e
kurth2u <kurth2u@etu.univ-lorraine.fr>
2022-05-10 15:24:59 +0200

    ajout getEtapeIG(Etape e)

--- src/twisk/outils/CorrespondanceEtapes.java
+++ src/twisk/outils/CorrespondanceEtapes.java
@@ -4,6 +4,7 @@ import twisk.monde.Etape;
 import twisk.mondeIG.EtapeIG;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class CorrespondanceEtapes {
     protected HashMap<EtapeIG,Etape> etapes;
@@ -18,6 +19,15 @@ public class CorrespondanceEtapes {
 
     public Etape getEtape(EtapeIG e){
         return etapes.get(e);
+    }
+
+    public EtapeIG getEtapeIG(Etape e){
+        for (HashMap.Entry<EtapeIG,Etape> ent : etapes.entrySet()){
+            if (Objects.equals(e,ent.getValue())){
+                return ent.getKey();
+            }
+        }
+        return null;
     }
 
     public void remove(EtapeIG e){

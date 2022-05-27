e90cd4da4545b474c2548b9010f031e1c45bc0d0
Victor Dallé (dalle5u) <victor.dalle8@etu.univ-lorraine.fr>
2022-05-27 23:40:45 +0200

    Nettoyage du code de VueClient.

--- src/twisk/vues/VueClient.java
+++ src/twisk/vues/VueClient.java
@@ -3,7 +3,6 @@ package twisk.vues;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import twisk.mondeIG.EtapeIG;
-import twisk.outils.CorrespondanceEtapes;
 import twisk.outils.TailleComposants;
 import twisk.simulation.Client;
 

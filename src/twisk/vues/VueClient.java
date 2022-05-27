fde7231b80b9916fc4877c38402f32ed53c9538f
Victor Dallé (dalle5u) <victor.dalle8@etu.univ-lorraine.fr>
2022-05-27 23:40:25 +0200

    Nettoyage du code de VueClient

--- src/twisk/vues/VueClient.java
+++ src/twisk/vues/VueClient.java
@@ -10,24 +10,13 @@ import twisk.simulation.Client;
 import java.util.Random;
 
 public class VueClient extends Circle {
-
-    private Client client;
-    private final EtapeIG etapeIG;
-
-    private int x;
-    private int y;
-    private final int radius = TailleComposants.getInstance().getRadiusClient();
-
     public VueClient(Client client, EtapeIG etapeIG) {
         super();
-        this.etapeIG = etapeIG;
         this.setCenterX(etapeIG.getPosX() + TailleComposants.getInstance().getLargeurActivite() - client.getRang()*25); // Partie droite de l'étape décalée de n rangs vers la gauche.
         this.setCenterY(etapeIG.getPosY() + 100);
-        this.setRadius(radius);
+        this.setRadius(TailleComposants.getInstance().getRadiusClient());
         Random random = new Random();
         this.setFill(Color.rgb(client.getNumeroClient()*12%256, client.getNumeroClient()*123%256, client.getNumeroClient()*1234%256));
-
-        //System.out.println(this);
     }
 
 }
